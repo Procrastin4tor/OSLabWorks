08ec4d0e7895b83a5f48c31d66768572350d3784
Procrastin4tor <66491243+Procrastin4tor@users.noreply.github.com>
2021-06-11 17:02:31 +0300

    Update client.c

--- lab12/client/client.c
+++ lab12/client/client.c
@@ -16,16 +16,16 @@
 int main(int argc, char *argv[]) 
 {
   struct sockaddr_in servaddr;
-  int sockfd = socket(PF_INET, SOCK_STREAM, 0); //создаём сокет
+  int sockfd = socket(PF_INET, SOCK_STREAM, 0);
 
   char *sep = strchr(argv[1], ':');
-  bzero(&servaddr, sizeof(servaddr)); // обнуляем структуру адреса сервера
-  servaddr.sin_family = AF_INET;//заполнение структуры адреса сервера
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
   servaddr.sin_port = htons(atoi(sep + 1)); 
   sep[0] = 0;
-  inet_aton(argv[1], &servaddr.sin_addr); //присваиваем ip нужный формат 
+  inet_aton(argv[1], &servaddr.sin_addr);
 
-  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) // логическое соединение сокета с сокетом сервера
+  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
   {
     printf("Can't connect to server\n");
     return 1;
@@ -50,23 +50,23 @@ int main(int argc, char *argv[])
     }
     else 
     {
-      write(sockfd, command, strlen(command) + 1); // отпраыляем серверу полнео имя файла
+      write(sockfd, command, strlen(command) + 1);
       long status;
-      int n = read(sockfd, &status, sizeof(status)); // получаем ответ
+      int n = read(sockfd, &status, sizeof(status));
       if (n == 0)
       {
         close(sockfd);
         printf("Connection lost\n");
         exit(1);
       }
-      else if (status == -1) // выводим что файла нет
+      else if (status == -1)
       {
         printf("File not found by server\n");
       }
       else
       {
           char filecon[1000];
-          printf("%s\n", status); // выводим содержимое файла
+          printf("%s\n", status); 
       }
     }
   }
